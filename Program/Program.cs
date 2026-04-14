86d7b981ae85d25c2f4fba6c298a18fc7e2b7048
hxun202 <kangsojin0918@gmail.com>
2026-04-14 21:44:47 +0900

    2026.4.14 가비지 컬렉션

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,34 +1,57 @@
 ﻿namespace Program
 {
+    public class Circle
+    {
+        public int x;
+        public int y;
+        public float radius;
+
+        public Circle()
+        {
+            Console.WriteLine("Created Circle");
+        }
+
+        ~Circle()
+        {
+            Console.WriteLine("Destroyed Circle");
+        }
+    }
+
     internal class Program
     {
+        static void Collide(Circle origin, Circle other)
+        {
+            float deltaX = origin.x - other.x;
+            float deltaY = origin.y - other.y;
+
+            if(deltaX*deltaX + deltaY * deltaY <= (origin.radius + other.radius) * (origin.radius + other.radius))
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+
         static void Main(string[] args)
         {
-            #region 박싱
-            // 값 타입을 참조 타입으로 변환하여 관리되는 힙 영역에 새로운 객체를
-            // 만들고 복사하는 과정입니다.
+            #region 매개 변수 한정자
+            // 인수가 함수에 전달되는 방식과 사용 규칙을 제어하는 한정자입니다.
 
-            //int count = 3;
+            Circle circle = new Circle();
 
-            //object clone = count;
+            circle.x = 5;
+            circle.y = 5;
+            circle.radius = 1.0f;
 
-            //Console.WriteLine("count : " + count);
-            //Console.WriteLine("clone : " + clone);
-            #endregion
+            Circle quadrant = new Circle();
 
-            #region 언박싱
-            // 관리되는 힙 영역에 있는 박싱되어 있는 객체에서 값을 꺼내 
-            // 값 타입으로 복사하는 과정입니다.
+            quadrant.x = 1;
+            quadrant.y = 2;
+            quadrant.radius = 1.0f;
 
-            //long experience = 2000;
-
-            //object address = experience;
-
-            //long data = (long)address;
-
-            //Console.WriteLine("experience : " + experience);
-            //Console.WriteLine("address : " + address);
-            //Console.WriteLine("data : " + data);
+            Collide(circle, quadrant);
 
             #endregion
         }
