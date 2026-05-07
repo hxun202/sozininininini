7fafa284fdb3c63bd3e4ce34b8705857a07a487e
hxun202 <kangsojin0918@gmail.com>
2026-05-07 21:43:55 +0900

    2026.5.7 인터페이스 분리 원칙

---
+++ Program/Weapon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    internal abstract class Weapon
+    {
+        protected int range;
+        protected int damage;
+
+        public abstract void Attack();
+    }
+}
--- Program/obj/Debug/net8.0/Program.AssemblyInfo.cs
+++ Program/obj/Debug/net8.0/Program.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Program")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ecb752b186c699a003679583dc8f7747595b6fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f39caeb48580fb3fbf5a27ff4cfa7188e38354d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Program")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Program")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
